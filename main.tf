3147757495912078461c4ce6ae4ffb65fa69e2a0
dozed-dev <dev.dozed@aleeas.com>
2025-09-05 22:01:25 +0300

    add 3 nodes in vpc

--- main.tf
+++ main.tf
@@ -23,3 +23,77 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-a"
 }
+
+# debian-12 image: fd8j3nge575bu7csn9sa
+# https://yandex.cloud/ru/marketplace/products/yc/debian-12
+
+resource "yandex_vpc_network" "k8s-network" {
+  name = "k8s-tf-network"
+}
+
+resource "yandex_vpc_subnet" "k8s-subnet" {
+  name           = "subnet1"
+  network_id     = yandex_vpc_network.k8s-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_security_group" "k8s-sg" {
+  name = "K8s cluster security group"
+  network_id = yandex_vpc_network.k8s-network.id
+
+  ingress {
+    protocol = "TCP"
+    description = "ssh"
+    port = 22
+  }
+}
+
+variable "node_count" {
+  type    = number
+  default = 3
+}
+
+resource "yandex_compute_disk" "k8s-node-boot-disks" {
+  count    = var.node_count
+  name     = "k8s-node-boot-disk-${count.index}"
+  type     = "network-ssd"
+  size     = "20"
+  image_id = "fd8j3nge575bu7csn9sa"
+}
+
+resource "yandex_compute_instance" "k8s-node-vms" {
+  count = var.node_count
+  name = "k8s-node-${count.index}"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.k8s-node-boot-disks[count.index].id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.k8s-subnet.id
+    nat       = true
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+  #security_group_ids = []
+
+  # https://yandex.cloud/ru/docs/compute/concepts/vm-metadata
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+output "internal_ip_addresses" {
+   value = [for x in yandex_compute_instance.k8s-node-vms : x.network_interface.0.ip_address]
+}
+
+output "external_ip_addresses" {
+   value = [for x in yandex_compute_instance.k8s-node-vms : x.network_interface.0.nat_ip_address]
+}
