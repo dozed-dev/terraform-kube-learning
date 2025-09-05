508a913217936a49c447b5083e54edfb200a6a86
dozed-dev <dev.dozed@aleeas.com>
2025-09-05 03:58:41 +0300

    Add nix shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Devshell for terraform";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [ opentofu jq ];
+      shellHook = ''
+        source ./setup_access.sh
+      '';
+    };
+  };
+}
