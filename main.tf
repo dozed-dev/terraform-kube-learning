450591fb3649646c2de73b9035ef4ead30b21342
dozed-dev <dev.dozed@aleeas.com>
2025-10-04 13:46:17 +0300

    split providers

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  # https://github.com/yandex-cloud-examples/yc-terraform-state
-  backend "s3" {
-    endpoint = "storage.yandexcloud.net"
-    use_lockfile = true
-    bucket = "playful-jarring.tf-state"
-    region = "ru-central1"
-    key = "terraform/terraform.tfstate"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
-  }
-  required_version = ">= 0.13"
-}
-
-provider "yandex" {
-  zone = "ru-central1-a"
-}
-
 resource "yandex_storage_bucket" "images-bucket" {
   bucket = "playful-jarring.images"
   max_size = 10 * pow(1024, 3) # GiB
