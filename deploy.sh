e6876343759e9cc71c681b6cd4c626d7b0fb903c
dozed-dev <dev.dozed@aleeas.com>
2025-10-22 18:19:02 +0300

    increase timeout

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ talosctl gen config talos-k8s-aws-tutorial https://$LB_IP.sslip.io:6443 \
 talosctl --talosconfig talosconfig config endpoint $CP_IP_1
 talosctl --talosconfig talosconfig config node $CP_IP_1
 talosctl --talosconfig talosconfig apply-config --insecure --nodes $CP_IP_1 --file controlplane.yaml
-sleep 30
+sleep 60
 talosctl --talosconfig talosconfig bootstrap
 sleep 30
 #talosctl --talosconfig talosconfig apply-config --nodes $CP_IP_1 --file controlplane.yaml
